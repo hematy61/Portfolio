c7ac5a8bc08d5bbec77d33bc5a21b470a7b6cfb2
Mohammad Hematy <hematy61@gmail.com>
2018-12-04 20:16:08 -0600

    -- add AboutMe.js Component   -- add AboutMe.scss   -- Modify settings.scss for borders   -- add settings.scss to Header styles

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import 'normalize.css';
 import './styles/base/base.scss';
 import Header from './components/Header/Header';
 import Intro from './components/Intro/Intro';
+import AboutMe from './components/AboutMe/AboutMe';
 
 
 const App = () => (
   <div>
     <Header />
     <Intro />
-    <h1 id='projects' >Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque iste dicta cupiditate vitae accusamus sed laudantium reprehenderit similique, impedit unde, pariatur soluta. Laborum vel, omnis voluptate cum neque excepturi debitis.</h1>
+    <AboutMe />
     <h1 >Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque iste dicta cupiditate vitae accusamus sed laudantium reprehenderit similique, impedit unde, pariatur soluta. Laborum vel, omnis voluptate cum neque excepturi debitis.</h1>
     <h1 >Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque iste dicta cupiditate vitae accusamus sed laudantium reprehenderit similique, impedit unde, pariatur soluta. Laborum vel, omnis voluptate cum neque excepturi debitis.</h1>
     <h1 >Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque iste dicta cupiditate vitae accusamus sed laudantium reprehenderit similique, impedit unde, pariatur soluta. Laborum vel, omnis voluptate cum neque excepturi debitis.</h1>
---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { 
+  about_me_container,
+  content_frame 
+} from "../../styles/components/AboutMe/AboutMe.scss";
+
+const AboutMe = () => (
+  <section id="about-me" className={about_me_container} >
+    <div className={content_frame} >
+      <p>
+        Mo is a graduate in Electronic Engineering and awarded the top student with the grade 18.5 / 20. 
+        He has developed a great interest in programing since the first days of university as he took a few mandatory courses in programing with C++ and MATLAB. 
+        His passion guide him through an adventure for Android and iOS programing with Swift. 
+        Finally he found web development is what he needs. 
+        He never forget the first <code><span>console</span><span>.log(</span>"Hello World!!!!"<span>)</span></code> and the excitement that took over him.
+        
+      </p>
+      <p>
+        As a self-taught developer, he has passed a great deal of challenges and still hungry to take more. 
+        He would prefer to stay an all-time junior developer to be open on learning. 
+      </p>
+    </div>
+  </section>
+);
+
+export default AboutMe;
