a27807acd0735f707e13a36efe391e4f3e88c225
Mohammad Hematy <hematy61@gmail.com>
2019-03-21 20:37:03 -0500

    add a new project and some fixes   -- change package json script   -- add weather Node and Express app project   -- fixed MongoDBSVGIcon console error   -- add some styles to the Project's links

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -60,6 +60,29 @@ const Projects = () => (
       </div>
     </div>
 
+    <div className={card}>
+      <div className={card_image_container}>
+        <a href="https://github.com/hematy61/weather-node-web-server-app" target='_blank' >
+          <img src={require('./weather-app.png')} alt="weather node and express app snapshot" />
+        </a>
+      </div>
+      <div className={subtitle}>
+        <h1>Weather Node and Express App</h1>
+        <h4><a href="https://mhemati-weather-node-app.herokuapp.com" target='_blank' >Click here to see it in action</a></h4>
+        <h4>NodeJS, Express, handlebars, request, hbs, env-cmd</h4>
+        <p>
+          This is a weather app which is using Node and Express in back-end to pull data from 
+          Mapbox nad Darksky APIs and serve the front-end pages.
+        </p>
+        <p>HTTP requests to Mapbox API and grab longitude and latitude</p>
+        <p>HTTP requests to Darksky API and grab weather for the location received through MApbox API</p>
+        <p>Fetch data from back end and represent them to user</p>
+        <p>Back-end HTTP requests are deployed through 'request' npm package</p>
+        <p>Front-end HTTP requests are deployed through Fetch API and Promises </p>
+        <p>Deployed on Heroku</p>
+      </div>
+    </div>
+
   </section>
 );
 
