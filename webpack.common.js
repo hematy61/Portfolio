39020c87378cbc3d4e6bd91fb0f2e01a473a9d34
Mohammad Hematy <hematy61@gmail.com>
2019-03-29 18:25:11 -0500

    change app.bundle.js file name to app.mainBundle.js as hostinger mixed it up with subdirectories

--- webpack.common.js
+++ webpack.common.js
@@ -12,7 +12,7 @@ module.exports = {
     })
   ],
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].mainBundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
