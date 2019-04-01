8b58912dfe0ed1ee30aa6dd5fc1c7b4b5ad9591c
Mohammad Hematy <hematy61@gmail.com>
2019-03-31 20:08:19 -0500

    change project's order

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -36,6 +36,30 @@ const Projects = () => (
 
     <div className={card}>
       <div className={card_image_container}>
+        <a href="https://github.com/hematy61/weather-node-web-server-app" target='_blank' >
+          <img src={require('./weather-app.png')} alt="weather node and express app snapshot" />
+        </a>
+      </div>
+      <div className={subtitle}>
+        <h1>Weather Node and Express App</h1>
+        <h4><a href="https://github.com/hematy61/weather-node-web-server-app" target='_blank' >Checkout GitHub Repository</a></h4>
+        <h4><a href="https://mhemati-weather-node-app.herokuapp.com" target='_blank' >Click here to see it in action</a></h4>
+        <h4>NodeJS, Express, handlebars, request, hbs, env-cmd</h4>
+        <p>
+          This is a weather app which is using Node and Express in back-end to pull data from
+          Mapbox nad Darksky APIs and serve the front-end pages.
+        </p>
+        <p>HTTP requests to Mapbox API and grab longitude and latitude</p>
+        <p>HTTP requests to Darksky API and grab weather for the location received through MApbox API</p>
+        <p>Fetch data from back end and represent them to user</p>
+        <p>Back-end HTTP requests are deployed through 'request' npm package</p>
+        <p>Front-end HTTP requests are deployed through Fetch API and Promises </p>
+        <p>Deployed on Heroku</p>
+      </div>
+    </div>
+
+    <div className={card}>
+      <div className={card_image_container}>
         <a href="https://hematy61.github.io/react-tv-package-configurator/" target='_blank' >
           <img src={require('./tv_package_configurator.gif')} alt="tv package configurator website snapshot" />
         </a>
@@ -62,30 +86,6 @@ const Projects = () => (
         <p>Using Yargs and Chalk</p>
         <p>Can take notes from command line and save them as a json file</p>
         <p>Edit, list all notes or find a specific note and remove a note</p>
-      </div>
-    </div>
-
-    <div className={card}>
-      <div className={card_image_container}>
-        <a href="https://github.com/hematy61/weather-node-web-server-app" target='_blank' >
-          <img src={require('./weather-app.png')} alt="weather node and express app snapshot" />
-        </a>
-      </div>
-      <div className={subtitle}>
-        <h1>Weather Node and Express App</h1>
-        <h4><a href="https://github.com/hematy61/weather-node-web-server-app" target='_blank' >Checkout GitHub Repository</a></h4>
-        <h4><a href="https://mhemati-weather-node-app.herokuapp.com" target='_blank' >Click here to see it in action</a></h4>
-        <h4>NodeJS, Express, handlebars, request, hbs, env-cmd</h4>
-        <p>
-          This is a weather app which is using Node and Express in back-end to pull data from 
-          Mapbox nad Darksky APIs and serve the front-end pages.
-        </p>
-        <p>HTTP requests to Mapbox API and grab longitude and latitude</p>
-        <p>HTTP requests to Darksky API and grab weather for the location received through MApbox API</p>
-        <p>Fetch data from back end and represent them to user</p>
-        <p>Back-end HTTP requests are deployed through 'request' npm package</p>
-        <p>Front-end HTTP requests are deployed through Fetch API and Promises </p>
-        <p>Deployed on Heroku</p>
       </div>
     </div>
 
